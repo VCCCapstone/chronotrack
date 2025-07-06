69894e12fdce97f087b6cfd897e46b9d7108889a
Yash Jani <yashjani.ca@gmail.com>
2025-07-06 02:41:41 -0400

    v2.2 5July25 final testing ci/cd30

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -28,7 +28,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "6e747e202ea26d1e0e9d242eebac2f41
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "87b643a431d437f6e3d76ff43bec0511",
+"flutter_bootstrap.js": "d74c7310d0cfbe3b4eae076a3dc28128",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
