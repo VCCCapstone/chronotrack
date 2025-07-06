cf7e59af74aa9ad4d3c622e3467835e8b5823eba
Yash Jani <yashjani.ca@gmail.com>
2025-07-06 02:53:27 -0400

    v2.2 5July25 final testing ci/cd31

--- lib/user/user_list_expense_page.dart
+++ lib/user/user_list_expense_page.dart
@@ -65,7 +65,7 @@ class _UserListExpensePageState extends State<UserListExpensePage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Your Submitted Expenses"),
-        backgroundColor: const Color.fromARGB(255, 245, 218, 10),
+        backgroundColor: const Color.fromARGB(255, 22, 245, 10),
         foregroundColor: Colors.white,
       ),
       body: _loading
